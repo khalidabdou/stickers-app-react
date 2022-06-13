0a049e3bcb664c5f51eb2b2a853d801006c8be06
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2022-06-13 10:12:33 +0100

    update

--- Client/src/services/http-common.js
+++ Client/src/services/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import env from "react-dotenv";
 
 export default axios.create({
-  baseURL: 'http://77.243.85.134:9000' ,
+  baseURL: env.REACT_APP_API ,
   headers: {
     'Content-Type': 'application/json',
     'Content-Type': 'multipart/form-data',
