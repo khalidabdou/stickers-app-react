11ecc5e29c9f9d352a145a9501f115fb3c2531bb
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2022-06-13 10:02:36 +0100

    Update http-common.js

--- Client/src/services/http-common.js
+++ Client/src/services/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import env from "react-dotenv";
 
 export default axios.create({
-  baseURL: 'http://77.243.85.134:3001' ,
+  baseURL: 'http://77.243.85.134:9000' ,
   headers: {
     'Content-Type': 'application/json',
     'Content-Type': 'multipart/form-data',
