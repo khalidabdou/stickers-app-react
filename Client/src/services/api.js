daeefb3ebbf463e703067b30a9b31794e243b88a
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2022-06-13 10:31:20 +0100

    Update api.js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,4 +1,4 @@
-const API='http://77.243.85.134:3001'
+const API='http://77.243.85.134:9000'
 const API_STICKERS= API+'/stickers/'
 const API_LANGUAGES= API+'/languages'
 const API_CATEGORIES= API+'/categories/'
